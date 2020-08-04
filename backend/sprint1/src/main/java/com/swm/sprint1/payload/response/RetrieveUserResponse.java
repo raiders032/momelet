d0b11eee92305c233b8b7374f6cd63cdef60d101
raiders032 <nameks@naver.com>
2020-08-04 16:24:36 +0900

    feature/back-restaurant-api APi url 수정

--- backend/sprint1/src/main/java/com/swm/sprint1/payload/response/RetrieveUserResponse.java
+++ backend/sprint1/src/main/java/com/swm/sprint1/payload/response/RetrieveUserResponse.java
@@ -11,14 +11,14 @@ public class RetrieveUserResponse {
     private Long id;
     private String email;
     private String name;
-    private String profile;
+    private String imageUrl;
     private List<String> categories;
 
-    public RetrieveUserResponse(Long id, String name, String email, String profile, List<String> categories) {
+    public RetrieveUserResponse(Long id, String name, String email, String imageUrl, List<String> categories) {
         this.id=id;
         this.name=name;
         this.email=email;
-        this.profile=profile;
+        this.imageUrl=imageUrl;
         this.categories=categories;
     }
 }
