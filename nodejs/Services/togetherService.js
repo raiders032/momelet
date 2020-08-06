0c98cfe499d2b40d7406381c8a73b896dc519507
jsjphone8 <jsjphone8@gmail.com>
2020-08-06 12:40:35 +0900

    feature/websocket-socketIO-Users도메인분리 togetherService.js 에서 이름이 불명확한 것들 개선

--- nodejs/Services/togetherService.js
+++ nodejs/Services/togetherService.js
@@ -1,45 +1,51 @@
 const SingleObject = require("../SingleObjects");
 
-const findUserLocation = (socketId, lat, long) => {
-  const aroundUser = [];
+const findAroundUsers = (mySocketId, lat, long) => {
+  const aroundUsers = [];
   const max_lat = lat + 0.0025;
   const min_lat = lat - 0.0025;
   const max_long = long + 0.0025;
   const min_long = long - 0.0025;
 
   // 클래스의 프로퍼티에 직접접근하고 있음
-  for (let key of SingleObject.UserList.canPlayGameUserList) {
-    const user = SingleObject.UserList.connectedUserList.get(key);
+  for (let socketId of SingleObject.UserList.canPlayGameUserList) {
+    const aroundUser = SingleObject.UserList.connectedUserList.get(socketId);
 
-    if (socketId !== key) {
+    if (mySocketId !== socketId) {
       if (
-        user["latitude"] >= min_lat &&
-        user["latitude"] <= max_lat &&
-        user["longitude"] >= min_long &&
-        user["longitude"] <= max_long
+        aroundUser["latitude"] >= min_lat &&
+        aroundUser["latitude"] <= max_lat &&
+        aroundUser["longitude"] >= min_long &&
+        aroundUser["longitude"] <= max_long
       ) {
-        aroundUser.push({
-          socketId: user["socketId"],
-          name: user["name"],
-          imageUrl: user["imageUrl"],
+        aroundUsers.push({
+          socketId: aroundUser["socketId"],
+          name: aroundUser["name"],
+          imageUrl: aroundUser["imageUrl"],
         });
       }
     }
   }
-  return aroundUser;
+  return aroundUsers;
 };
 
 // 같이하기
 const togetherService = (socket, msg) => {
   var echo = "together 이벤트. 받은 msg: " + msg;
   console.log(echo);
-  const msgSplit = msg.split(" ");
-  const latitude = msgSplit[0];
-  const longitude = msgSplit[1];
 
-  const aroundUser = findUserLocation(socket.id, latitude, longitude);
+  // 수정되어야 하는 부분. 이제 msg의 형식을 JSON으로 통일할 예정이기 때문
+  // const msgSplit = msg.split(" ");
+  // const latitude = msgSplit[0];
+  // const longitude = msgSplit[1];
 
-  return aroundUser;
+  // 테스팅용. index.html 의 형식에 맞춤.
+  const latitude = msg[0];
+  const longitude = msg[1];
+
+  const aroundUsers = findAroundUsers(socket.id, latitude, longitude);
+
+  return aroundUsers;
 };
 
 module.exports = {
