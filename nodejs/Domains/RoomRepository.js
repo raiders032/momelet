9de485eea0522f510c45822c6ba09b3251ad70d0
raiders032 <nameks@naver.com>
2020-08-07 18:28:28 +0900

    back RoomRepository 구현

--- nodejs/Domains/RoomRepository.js
+++ nodejs/Domains/RoomRepository.js
@@ -3,19 +3,24 @@ class RoomRepository {
     this.roomRepository = new Map();
   }
 
-  addRoom = (room) => {
-    if (this.roomRepository.has(room.roomName))
-      throw new Error("같은 이름의 방이 이미 존재합니다.");
-    this.roomRepository.set(room.roomName, room);
+  addRoom = (hostSocketId) => {
+    const roomName = hostSocketId + Date.now();
+    const newRoom = new Room(roomName, hostSocketId);
+
+    if (this.roomRepository.has(room.roomName)) return null;
+
+    this.roomRepository.set(newRoom);
+    return roomName;
   };
 
-  deleteRoom = (room) => {
-    if (!this.roomRepository.has(room.roomName))
-      throw new Error("해당 방이 이미 존재하지 않습니다.");
-    this.roomRepository.delete(room.roomName);
+  deleteRoom = (roomName) => {
+    if (!this.roomRepository.has(roomName)) return false;
+    this.roomRepository.delete(roomName);
+    return true;
   };
 
   findRoomByRoomName = (roomName) => {
+    if (!this.roomRepository.has(roomName)) return false;
     return this.roomRepository.get(roomName);
   };
 }
