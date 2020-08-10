ddc37d2cbea6333e6d1e00f58124882bd39de337
jsjphone8 <jsjphone8@gmail.com>
2020-08-10 17:46:45 +0900

    충돌난 부분 재수정

--- nodejs/SingleObjects.js
+++ nodejs/SingleObjects.js
@@ -4,8 +4,5 @@ const UserRepository = new userRepositoryDomain.UserRepository();
 const roomRepositoryDomain = require("./Domains/Roomrepository");
 const RoomRepository = new roomRepositoryDomain.RoomRepository();
 
-module.exports.app = app;
-module.exports.server = server;
-module.exports.io = io;
 module.exports.UserRepository = UserRepository;
 module.exports.RoomRepository = RoomRepository;
