3959d5de98196f95a9fa45d0f24610c4fb642eac
jsjphone8 <jsjphone8@gmail.com>
2020-08-12 17:38:27 +0900

    gameStart 서버와 통신 준비 완료

--- nodejs/Services/game/gameStartService.js
+++ nodejs/Services/game/gameStartService.js
@@ -1,13 +1,27 @@
 const axios = require("axios");
 const SingleObject = require("../../SingleObjects");
 
-const getCard = async () => {
-  const cards = await axios.get(process.env.DATA_STORAGE_URL, {
-    params: {
+const getCard = async (users, myId) => {
+  let id = "";
+  for(let i = users.length; i > 0; i--){
+    if(i === 1) id += users[i].id;
+    else id += users[i].id + ",";
+  }
 
+  const {longitude, latitude} = SingleObject.UserRepository.findById(myId);
+  const cards = await axios.get(process.env.DATA_STORAGE_URL + "api/v1/restaurants7", {
+    params: {
+      id,
+      longitude,
+      latitude,
+      radius: 0.01,
     }
   }); // array
+
+  return cards;
 };
+
+
 const gameStartService = (socket, msg) => {
   var echo = "gameStart 이벤트. 받은 msg: " + msg;
   console.log(echo);
@@ -21,22 +35,22 @@ const gameStartService = (socket, msg) => {
   const room = SingleObject.RoomRepository.findByRoomName(roomName);
 
   if (room !== false && room.getHostId() === id) {
-    // 1. data fetch
-    const cards = await getCard();
-    // 2. update retMsg
+    const users = room.getUserList();
+    const cards = await getCard(users, id);
     if(cards.legnth === 7){
       retMsg.status = "ok";
       retMsg.restaurants = cards;
+      retMsg = JSON.stringify(retMsg);
+  
+      users
+        .filter((user) => user.getCanReceive() && id !== user.id)
+        .forEach((user) => {
+          socket.to(user.socketId).emit("gameRoomUpdate"), retMsg;
+        });
     }
-    // 3. JSON stringify retMsg
-    retMsg = JSON.stringify(retMsg);
-        
-    const users = room.getUserList();
-    users
-      .filter((user) => user.getCanReceive() && id !== user.id)
-      .forEach((user) => {
-        socket.to(user.socketId).emit("gameRoomUpdate"), retMsg;
-      });
+    else {
+      retMsg = JSON.stringify(retMsg);
+    }
   }
   return retMsg;
 };
