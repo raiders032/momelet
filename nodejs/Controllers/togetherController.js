0004b6b76d93872349afa5e0881d0e1d2d1e0588
jsjphone8 <jsjphone8@gmail.com>
2020-08-13 11:11:27 +0900

    함수명을 좀 더 명확하게 변경

--- nodejs/Controllers/togetherController.js
+++ nodejs/Controllers/togetherController.js
@@ -2,24 +2,24 @@ const service = require("../Services/index");
 
 const togetherController = (socket) => {
   // 같이하기;
-  socket.on("together", (msg, fn) => {
+  socket.on("together", (msg, ack) => {
     const ret = service.togetherService(socket, msg);
     // socket.emit("together", ret);
-    fn(ret);
+    ack(ret);
   });
 
   // 같이하기-초대하기
-  socket.on("togetherInvite", (msg, fn) => {
+  socket.on("togetherInvite", (msg, ack) => {
     const ret = service.togetherInviteService(socket, msg);
     //socket.emit("togetherInvite", ret);
-    fn(ret);
+    ack(ret);
   });
 
   // 초대수락
-  socket.on("togetherInvitation", (msg, fn) => {
+  socket.on("togetherInvitation", (msg, ack) => {
     const ret = service.togetherInvitationService(socket, msg);
     // socket.emit("togetherInvitation", ret);
-    fn(ret);
+    ack(ret);
   });
 };
 
--- nodejs/Services/disconnect/disconnectService.js
+++ nodejs/Services/disconnect/disconnectService.js
@@ -3,15 +3,13 @@ const SingleObject = require("../../SingleObjects");
 const disconnectService = (socket) => {
   console.log(
     "user disconnected " +
-      SingleObject.UserRepository.findById(
-        SingleObject.UserRepository.findBySocketId(socket.id).id
-      )
+      SingleObject.UserRepository.findBySocketId(socket.id).id
   );
 
   // 클래스의 프로퍼티에 직접 접근하고 있음
   SingleObject.UserRepository.delete(socket.id);
 
-  const userId = SingleObject.UserRepository.findAll();
+  const userId = SingleObject.UserRepository.findAll().map((user) => user.id);
   console.log("나간 후 유저리스트: " + userId);
 };
 
