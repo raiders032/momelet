873051a1ccb5ed2d9553e9c820c00f66adb28918
jsjphone8 <jsjphone8@gmail.com>
2020-08-21 18:58:26 +0900

    로깅 메시지 추가 수정

--- nodejs/Services/disconnect/disconnectService.js
+++ nodejs/Services/disconnect/disconnectService.js
@@ -2,14 +2,6 @@ const SingleObject = require("../../SingleObjects");
 const { logger } = require("../../logger");
 
 const disconnectService = (socket) => {
-  console.log(
-    "user disconnected " +
-      SingleObject.UserRepository.findBySocketId(socket.id).id
-  );
-  logger.info(
-    "user disconnected " +
-      SingleObject.UserRepository.findBySocketId(socket.id).id
-  );
   // 클래스의 프로퍼티에 직접 접근하고 있음
   SingleObject.UserRepository.delete(socket.id);
 
