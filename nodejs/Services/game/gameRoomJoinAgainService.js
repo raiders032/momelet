ec766eb424547be394cf7cf65ec30afbc80b73d6
jsjphone8 <jsjphone8@gmail.com>
2020-09-03 20:15:23 +0900

    gameRoomJoinAgain 예외 처리 완료

--- nodejs/Services/game/gameRoomJoinAgainService.js
+++ nodejs/Services/game/gameRoomJoinAgainService.js
@@ -5,16 +5,26 @@ const gameRoomJoinAgainService = (socket, msg) => {
   var echo = "gameRoomJoinAgain. msg: " + msg;
   logger.info(echo);
 
-  const { id, roomName } = JSON.parse(msg);
-  const room = SingleObject.RoomRepository.findByRoomName(roomName);
-  const user = SingleObject.UserRepository.findById(id);
-
   let retMsg = {
     status: "fail",
     gameUserList: null,
   };
+  try {
+    const parsedMsg = JSON.parse(msg);
+    id = parsedMsg.id;
+    roomName = parsedMsg.roomName;
+  } catch (err) {
+    logger.error("gameRoomJoinAgain Msg parse error: " + err);
+    return JSON.stringify(retMsg);
+  }
 
-  if (room.getIsStarted() === false && room.findUserById(user.getId())) {
+  const room = SingleObject.RoomRepository.findByRoomName(roomName);
+  if (
+    room !== false &&
+    room.getIsStarted() === false &&
+    room.findUserById(user.getId())
+  ) {
+    const user = SingleObject.UserRepository.findById(id);
     user.updateCanReceive(true);
     retMsg.status = "ok";
     retMsg.gameUserList = room
@@ -22,8 +32,7 @@ const gameRoomJoinAgainService = (socket, msg) => {
       .filter((user) => user.getCanReceive())
       .map((user) => {
         const { id, name, imageUrl } = user;
-        const userDto = { id, name, imageUrl };
-        return userDto;
+        return { id, name, imageUrl };
       });
   }
 
