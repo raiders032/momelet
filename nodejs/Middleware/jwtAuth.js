aac1dd9224048671f5a90ec67b95269627cf5feb
jsjphone8 <jsjphone8@gmail.com>
2020-09-09 11:59:27 +0900

    socketio-jwt 라이브러리를 이용해서 작성 중

--- nodejs/Middleware/jwtAuth.js
+++ nodejs/Middleware/jwtAuth.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 import logger from "../logger.js";
 
-export default async (req, res, next) => {
-  const token = req.body;
-  console.log("어케 통과했니");
-  try {
-    const {
-      data: { success },
-    } = await axios.post(process.env.SERVER_URL + "/api/v1/auth/validation", {
-      jwt: token,
-    });
+// export default async (socket, next) => {
+//   const token = socket.headers.Autorization;
+//   console.log("어케 통과했니");
+//   try {
+//     const {
+//       data: { success },
+//     } = await axios.post(process.env.SERVER_URL + "/api/v1/auth/validation", {
+//       jwt: token,
+//     });
 
-    if (success === false) {
-      return res.status(401).send("unknown token");
-    }
-    next();
-  } catch (err) {
-    logger.error("jwt auth error: " + err);
-  }
-  next();
-};
+//     if (success === false) {
+//       return res.status(401).send("unknown token");
+//     }
+//     next();
+//   } catch (err) {
+//     logger.error("jwt auth error: " + err);
+//   }
+//   next();
+// };
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,20 +1,43 @@
 import express from "express";
 import logger from "./logger.js";
-import webSocket from "./socket.js";
+import SocketIO from "socket.io";
 import dotenv from "dotenv";
-// import jwtAuth from "./Middleware/jwtAuth.js";
+import socketioJwt from "socketio-jwt";
+import * as controller from "./Controllers/index.js";
+import userFilter from "./Middleware/userFilter.js";
 dotenv.config();
 
 const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(jwtAuth);
-
 const server = app.listen(process.env.PORT, () => {
   logger.info("Server started. listening on *:3000.");
 });
+
+// webSocket(server, app);
+
+const io = SocketIO(server, {
+  //path: "/momulet",
+  serverClient: false, // 클라이언트 파일 제공 여부
+  pingInterval: 20000, // 20초에 한 번씩 핑 보내기
+  pingTimeout: 10000, // 10초 동안 퐁 기다리기
+});
+
+io.use(
+  socketioJwt.authorize({
+    secret: "secret key",
+    handshake: true,
+    auth_header_required: true,
+  })
+);
+io.use((socket, next) => {
+  userFilter(io, socket, next);
+});
+
+io.on("connection", (socket) => {
+  controller.togetherController(socket);
+  controller.gameController(socket);
+  controller.disconnectController(socket);
+});
+
 app.set("server", server);
-
-webSocket(server, app);
-
+app.set("io", io);
 export default app;
