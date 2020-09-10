ad33f4cd3329704f2fb73b9404837c034f628517
raiders032 <nameks@naver.com>
2020-09-10 15:38:13 +0900

    토큰 만료기간 설정

--- backend/sprint1/src/main/java/com/swm/sprint1/security/TokenProvider.java
+++ backend/sprint1/src/main/java/com/swm/sprint1/security/TokenProvider.java
@@ -14,10 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Transactional
 @RequiredArgsConstructor
