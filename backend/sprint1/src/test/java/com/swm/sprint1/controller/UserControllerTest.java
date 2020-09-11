514f08dcece8f1f4195688b19cf83d7e3883ad79
raiders032 <nameks@naver.com>
2020-09-11 16:26:19 +0900

    유저 정보 조회 테스트 수정 완료

--- backend/sprint1/src/test/java/com/swm/sprint1/controller/UserControllerTest.java
+++ backend/sprint1/src/test/java/com/swm/sprint1/controller/UserControllerTest.java
@@ -1,10 +1,12 @@
 package com.swm.sprint1.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swm.sprint1.domain.AuthProvider;
 import com.swm.sprint1.domain.Category;
 import com.swm.sprint1.domain.User;
 import com.swm.sprint1.exception.ResourceNotFoundException;
+import com.swm.sprint1.payload.response.ApiResponse;
 import com.swm.sprint1.repository.category.CategoryRepository;
 import com.swm.sprint1.repository.user.UserRepository;
 import com.swm.sprint1.security.Token;
@@ -23,16 +25,20 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -51,18 +57,21 @@ public class UserControllerTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private final Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
 
     private User user;
 
-    private String  jwtToken;
+    private String jwtToken;
 
     @Before
-    public void init(){
+    public void init() {
         user = User.builder()
-                .name("kjy")
-                .email("kjy@test.com")
-                .imageUrl("imageurl")
+                .name("변경전이름")
+                .email("before@before.com")
+                .imageUrl("imageUrlBefore")
                 .provider(AuthProvider.local)
                 .providerId("test")
                 .userCategories(new HashSet<>())
@@ -76,11 +85,11 @@ public class UserControllerTest {
         UserPrincipal userPrincipal = UserPrincipal.create(user);
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
         List<Token> tokens = tokenProvider.createToken(authentication);
-        jwtToken = "Bearer "+tokens.get(0).getJwtToken();
+        jwtToken = "Bearer " + tokens.get(0).getJwtToken();
     }
 
     @After
-    public void clear(){
+    public void clear() {
         logger.info("유저 리포지토 비우기");
         userRepository.deleteAll();
     }
@@ -89,7 +98,7 @@ public class UserControllerTest {
     public void 유저정보수정() throws Exception {
         //given
         logger.info("유저정보수정 테스트 시작");
-        String url = "/api/v1/users/"+ user.getId();
+        String url = "/api/v1/users/" + user.getId();
         String name = "변경된이름";
         String categories = "한식,일식,중식";
         MockMultipartFile file = new MockMultipartFile("imageFile", "test.jpg", "image/jpg", "asdasdasd".getBytes());
@@ -113,7 +122,7 @@ public class UserControllerTest {
     @Test
     public void 유저정보수정_파일을보내지않았을때() throws Exception {
         //given
-        String url = "/api/v1/users/"+ user.getId();
+        String url = "/api/v1/users/" + user.getId();
         String name = "변경된이름";
         String categories = "한식,일식,중식";
 
@@ -135,7 +144,7 @@ public class UserControllerTest {
     @Test
     public void 유저정보수정_이름을보내지않은경우() throws Exception {
         //given
-        String url = "/api/v1/users/"+ user.getId();
+        String url = "/api/v1/users/" + user.getId();
         String name = "";
         String categories = "한식,일식,중식";
         MockMultipartFile file = new MockMultipartFile("imageFile", "test.jpg", "image/jpg", "asdasdasd".getBytes());
@@ -161,7 +170,7 @@ public class UserControllerTest {
     @Test
     public void 유저정보수정_카테고리를보내지않은경우() throws Exception {
         //given
-        String url = "/api/v1/users/"+ user.getId();
+        String url = "/api/v1/users/" + user.getId();
         String name = "변경된이름";
         String categories = "";
         MockMultipartFile file = new MockMultipartFile("imageFile", "test.jpg", "image/jpg", "asdasdasd".getBytes());
@@ -191,25 +200,27 @@ public class UserControllerTest {
         String url = "/api/v1/users/me";
 
         //when
-        ResultActions result = mockMvc.perform(get(url).header("authorization", jwtToken));
+        MvcResult result = mockMvc.perform(get(url).header("authorization", jwtToken))
+                .andExpect(status().isOk())
+                .andReturn();
 
         //then
-        result
-                .andExpect(jsonPath("$.success").value("true"))
-                .andExpect(jsonPath("$.data.userInfo.name").value(user.getName()))
-                .andExpect(jsonPath("$.data.userInfo.imageUrl").value(user.getImageUrl()))
-                .andExpect(jsonPath("$.data.userInfo.email").value(user.getEmail()))
-                .andExpect(jsonPath("$.data.userInfo.categories.양식").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.중식").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.고기").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.일식").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.치킨").value("0"))
-                //.andExpect(jsonPath("$.data.userInfo.categories.찜,탕").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.한식").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.세계음식").value("0"))
-                //.andExpect(jsonPath("$.data.userInfo.categories.곱,대창").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.주점").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.분식").value("0"))
-                .andExpect(jsonPath("$.data.userInfo.categories.패스트푸드").value("0"));
+        String content = result.getResponse().getContentAsString();
+        ApiResponse apiResponse = objectMapper.readValue(content, ApiResponse.class);
+        HashMap<String, Integer> categoires = (HashMap<String, Integer>) ((HashMap<String, Object>) apiResponse.getData().get("userInfo")).get("categories");
+
+        assertThat(apiResponse.isSuccess()).isTrue();
+        assertThat(categoires.get("양식")).isEqualTo(0);
+        assertThat(categoires.get("중식")).isEqualTo(0);
+        assertThat(categoires.get("고기")).isEqualTo(0);
+        assertThat(categoires.get("일식")).isEqualTo(0);
+        assertThat(categoires.get("치킨")).isEqualTo(0);
+        //assertThat(categoires.get("찜,탕")).isEqualTo(0);
+        assertThat(categoires.get("한식")).isEqualTo(0);
+        assertThat(categoires.get("세계음식")).isEqualTo(0);
+        //assertThat(categoires.get("곱,대창")).isEqualTo(0);
+        assertThat(categoires.get("주점")).isEqualTo(0);
+        assertThat(categoires.get("분식")).isEqualTo(0);
+        assertThat(categoires.get("패스트푸드")).isEqualTo(0);
     }
 }
