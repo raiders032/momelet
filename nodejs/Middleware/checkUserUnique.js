72bfc0f450fbf3813229891376a62ce126f8b32e
jsjphone8 <jsjphone8@gmail.com>
2020-09-15 19:58:09 +0900

    미들웨어 테스트 추가

--- nodejs/Middleware/checkUserUnique.js
+++ nodejs/Middleware/checkUserUnique.js
@@ -4,14 +4,18 @@ import logger from "../logger.js";
 export default (io, socket, next) => {
   socket.handshake.query.id = parseInt(socket.handshake.query.id);
   const { id } = socket.handshake.query;
+  // const { socketId } = SingleObject.UserRepository.findById(id);
+  // console.log("소켓 아이디: " + socketId);
+  console.log("구조 알아보기" + io.sockets);
+  for (let i in io.sockets) {
+    console.log(i);
+    console.log(io.sockets[i]);
+  }
   if (SingleObject.UserRepository.existById(id)) {
     // const user = SingleObject.UserRepository.findById(id);
     logger.info(id + " is overlapped user. Disconnect exsisting user");
-    const { socketId } = SingleObject.UserRepository.findById(id);
-    console.log("소켓 아이디: " + socketId);
     const user =
       io.sockets.connected[SingleObject.UserRepository.findById(id).socketId];
-    console.log("구조 알아보기" + io.sockets.connected);
     if (user !== undefined) user.disconnect(true);
     SingleObject.UserRepository.delete(socketId);
   }
--- nodejs/Services/disconnect/disconnectService.js
+++ nodejs/Services/disconnect/disconnectService.js
@@ -3,8 +3,9 @@ import logger from "../../logger.js";
 
 export default (socket) => {
   // 클래스의 프로퍼티에 직접 접근하고 있음
+  console.log("소켓: " + socket.toString());
+  console.log("소켓쩜아이디: " + socket.id);
   const user = SingleObject.UserRepository.findBySocketId(socket.id);
-  console.log("소켓: " + socket);
   if (user.joinedRoomName !== null) {
     SingleObject.RoomRepository.findByRoomName(user.joinedRoomName).deleteUser(
       user
