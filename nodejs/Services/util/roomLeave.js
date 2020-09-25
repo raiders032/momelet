2f7cf262107ba57568ef8aa20aef2f9a12eb5fa1
jsjphone8 <jsjphone8@gmail.com>
2020-09-25 15:53:00 +0900

    room이 없을 때 예외 던지는 부분 추가

--- nodejs/Services/util/roomLeave.js
+++ nodejs/Services/util/roomLeave.js
@@ -1,7 +1,11 @@
 import { RoomRepository } from "../../SingleObjects.js";
 import gameRoomUpdateService from "../game/gameRoomUpdateService.js";
+import { ERR_ROOM_NOT_FOUND } from "../../Errors/RepositoryError";
 
 export default (user, room) => {
+  if (room === false) {
+    throw new ERR_ROOM_NOT_FOUND();
+  }
   user.socket.leave(room.getRoomName());
   user.updateJoinedRoomName(null);
   if (room.deleteUser(user) > 0) {
