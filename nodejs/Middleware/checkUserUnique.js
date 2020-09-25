f386ae868ca307a5ce0902a27f532cea91891742
jsjphone8 <jsjphone8@gmail.com>
2020-09-25 17:57:11 +0900

    user connect logging 수정

--- nodejs/Middleware/checkUserUnique.js
+++ nodejs/Middleware/checkUserUnique.js
@@ -6,7 +6,6 @@ export default (socket) => {
   const { id } = socket.handshake.query;
 
   if (SingleObject.UserRepository.existById(id)) {
-    // const user = SingleObject.UserRepository.findById(id);
     logger.info(id + " is overlapped user. Disconnect exsisting user");
     const user = SingleObject.UserRepository.findById(id);
 
@@ -14,6 +13,5 @@ export default (socket) => {
   }
   SingleObject.UserRepository.add(socket, socket.handshake.query);
 
-  const userList = SingleObject.UserRepository.findAll().map((user) => user.id);
-  logger.info("a user connected. id: " + id + ", userList: (" + userList + ")");
+  logger.info("a user connected. id: " + id);
 };
