81f398738b9d09e3d2cfb8627563b84e34a7caa1
Changun Ha <giponge@gmail.com>
2020-10-19 16:25:39 +0900

    ios publish

--- reactNative/screen/Alone/Main/MainContainer.js
+++ reactNative/screen/Alone/Main/MainContainer.js
@@ -181,7 +181,7 @@ export default ({ navigation, route }) => {
     let latitude;
     let longitude;
 
-    if (!tmpConnect) {
+    if (tmpConnect) {
       latitude = 37.5447048;
       longitude = 127.0663154;
     } else {
@@ -240,7 +240,7 @@ export default ({ navigation, route }) => {
     let latitude;
     let longitude;
 
-    if (!tmpConnect) {
+    if (tmpConnect) {
       latitude = 37.5447048;
       longitude = 127.0663154;
     } else {
